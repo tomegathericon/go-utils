0a9986e0cd368f7a33165f676ef42ca9988ee6ce
Abhishek Kapoor <kapoor.abhishek@outlook.com>
2024-06-04 14:26:11 +0530

    Updated version

--- pkg/tracing4go/tracer/constants.go
+++ pkg/tracing4go/tracer/constants.go
@@ -2,5 +2,5 @@ package tracer
 
 type constants string
 
-const version constants = "v0.0.4"
+const version constants = "v0.1.0"
 const tracerName constants = "github.com/tomegathericon/tracing4go/tracer"
